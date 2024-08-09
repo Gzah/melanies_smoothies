202c0324610136e80866c7d8f6f1b21ef76c3b1a
Gzah <Gzah@users.noreply.github.com>
2024-08-08 22:52:51 -0400

    Update streamlit_orders_app.py
    Update for SniS

--- streamlit_orders_app.py
+++ streamlit_orders_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 
@@ -8,7 +8,9 @@ from snowflake.snowpark.functions import col, when_matched
 st.title(":cup_with_straw: Pending Smoothie Orders! :cup_with_straw:")
 st.write("""Orders that need to filled.""")
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.orders").filter(col('ORDER_FILLED')==0).collect()
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
